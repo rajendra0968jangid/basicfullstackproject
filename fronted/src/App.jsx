a5c2a8d4de6c8bce1868ac417e677df865787300
rajendra0968jangid <rajendra0968jangid@gmail.com>
2024-06-15 22:32:12 +0530

    routes add

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Product from "./Components/Product";
 function App() {
   return (
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Product />} />
+      </Routes>
+    </Router>
   );
 }
 
